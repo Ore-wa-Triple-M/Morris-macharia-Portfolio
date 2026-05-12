fde5a66039e3f0bdd26c5005a4478eea20678ef6
Ore-wa-Triple-M <machariamorris977@gmail.com>
2026-05-12 16:08:18 +0300

    Update home.js

--- home.js
+++ home.js
@@ -5,14 +5,14 @@ mainBtn.addEventListener('click', (e) => {
     e.preventDefault(); 
     clickCount++;
 
-    if (clickCount === 1) {
+    if (clickCount === 2) {
         // First click changes text to your first condition
         mainBtn.innerText = "Do you really want to know?";
         mainBtn.style.transform = "scale(1.5)";
         mainBtn.style.backgroundColor = "#00f2ff"; // Optional: change color to signal progress
         mainBtn.style.color = "#000";
     }  
-    else if (clickCount === 2) {
+    else if (clickCount === 3) {
         // Third click performs the actual redirect
         window.location.href = "about.html";
     }
