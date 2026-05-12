cbbe3e9e2d0955680fac83ea18ba4ea8934fd694
Ore-wa-Triple-M <machariamorris977@gmail.com>
2026-05-12 15:56:17 +0300

    Update home.js

--- home.js
+++ home.js
@@ -10,19 +10,6 @@ mainBtn.addEventListener('click', (e) => {
         // First click: Increase size and change text
         mainBtn.innerText = "Do you really want to know?";
         mainBtn.style.transform = "scale(1.5)";
-        
-    } else if (clickCount === 2) {
-        // Second click: Change text and spawn the "Yes" button
-        mainBtn.style.backgroundColor = "transparent";
-        mainBtn.style.border = "none";
-        mainBtn.style.cursor = "default";
-        mainBtn.style.fontSize = "40px";
-        mainBtn.style.color = "#00ffe4"; // Keeps your neon theme
-        mainBtn.style.transform = "scale(1)"; // Reset size so it's readable
-
-
-    } else if (clickCount === 4) {
-        // The final click
         window.location.href = "about.html";
     }
 });
