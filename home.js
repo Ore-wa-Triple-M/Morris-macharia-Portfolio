3f8d022a27156ab2be5e0b0ff693aa0a258bc2bd
Ore-wa-Triple-M <machariamorris977@gmail.com>
2026-05-12 15:53:18 +0300

    Update home.js

--- home.js
+++ home.js
@@ -21,28 +21,6 @@ mainBtn.addEventListener('click', (e) => {
         mainBtn.style.color = "#00ffe4"; // Keeps your neon theme
         mainBtn.style.transform = "scale(1)"; // Reset size so it's readable
 
-        // Check if the Yes button already exists so we don't spawn 100 of them
-        if (!document.getElementById('yesBtn')) {
-            const yesBtn = document.createElement('button');
-            yesBtn.id = 'yesBtn';
-            yesBtn.innerText = "YES";
-            yesBtn.style.marginLeft = "20px";
-            yesBtn.style.color= "white";
-            yesBtn.style.backgroundColor = " hsla(0, 100.00%, 40.00%, 0.90)"
-
-            // When they click 'Yes', it counts as the 3rd step
-            yesBtn.onclick = (e) => {
-                e.preventDefault();
-                clickCount = 3; 
-                mainBtn.innerText = "Click me again...";
-                mainBtn.style.backgroundColor = " hsl(174, 100%, 50%)";
-                mainBtn.style.cursor = "pointer";
-                mainBtn.style.color = " hsl(261, 100%, 27%)";
-                mainBtn.style.transform = "scale(1.5)"; // Reset size so it's read
-                yesBtn.remove(); 
-            };
-            container.appendChild(yesBtn);
-        }
 
     } else if (clickCount === 4) {
         // The final click
